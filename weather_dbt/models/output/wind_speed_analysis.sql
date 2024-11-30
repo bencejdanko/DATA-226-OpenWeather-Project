5f1abd77886d54d135b061b3db078fc203563cd5
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2024-11-29 17:56:59 -0800

    config: added ts and avg for each day

--- weather_dbt/models/output/wind_speed_analysis.sql
+++ weather_dbt/models/output/wind_speed_analysis.sql
@@ -1,7 +1,9 @@
 WITH WindSpeedData AS (
     SELECT 
         w.WIND_SPEED,
-        c."Name" AS CITY_NAME
+        c."Name" AS CITY_NAME,
+        w.DATE_TIME,
+        DATE_TRUNC('DAY', w.DATE_TIME) AS DAY
     FROM
         {{ source('openweather', 'WEATHER_FACT_TABLE') }} w
     JOIN
@@ -9,7 +11,16 @@ WITH WindSpeedData AS (
     ON
         w.CITY_ID = c.CITY_ID
     WHERE
-        w.WIND_SPEED IS NOT NULL 
+        w.WIND_SPEED IS NOT NULL
 )
-SELECT *
+SELECT 
+    CITY_NAME,
+    DAY,
+    AVG(WIND_SPEED) AS AVG_WIND_SPEED
 FROM WindSpeedData
+GROUP BY 
+    CITY_NAME,
+    DAY
+ORDER BY 
+    CITY_NAME,
+    DAY

4670b3fa838ee8cbff609f0f0d0cb5f08e22d3a1
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2024-11-29 19:33:00 -0800

    no longer shows error with duplicate record

--- weather_dbt/snapshots/extreme_weather_events_snapshot.sql
+++ weather_dbt/snapshots/extreme_weather_events_snapshot.sql
@@ -3,18 +3,17 @@
 {{
   config(
     target_schema='snapshot',
-    unique_key='CITY_NAME',
+    unique_key='DAY',
     strategy='timestamp',
-    updated_at='DATE_TIME',
+    updated_at='DAY',
     invalidate_hard_deletes=True
   )
 }}
 
 WITH Extreme_Weather_Events AS (
     SELECT
-        w.CITY_ID,
         c."Name" AS CITY_NAME,
-        w.DATE_TIME,
+        DATE_TRUNC('DAY', w.DATE_TIME) AS DAY,
         ((w.TEMP - 273.15) * 9/5 + 32) AS TEMP_FAHRENHEIT,
         w.WIND_SPEED,
         CASE
@@ -35,10 +34,22 @@ WITH Extreme_Weather_Events AS (
         OR w.WIND_SPEED > 20
 )
 SELECT
-    *
-FROM
+    CITY_NAME,
+    DAY,
+    AVG(TEMP_FAHRENHEIT) AS AVG_TEMP_FAHRENHEIT,
+    AVG(WIND_SPEED) AS AVG_WIND_SPEED,
+    CASE
+        WHEN AVG(TEMP_FAHRENHEIT) > 100 THEN 'Extreme Heat'
+        WHEN AVG(TEMP_FAHRENHEIT) < 32 THEN 'Extreme Cold'
+        WHEN AVG(WIND_SPEED) > 20 THEN 'High Winds'
+        ELSE 'Normal'
+    END AS WEATHER_ALERT
+FROM 
     Extreme_Weather_Events
-ORDER BY
-    DATE_TIME, CITY_NAME
+GROUP BY 
+    CITY_NAME,
+    DAY
+ORDER BY 
+    DAY
 
 {% endsnapshot %}

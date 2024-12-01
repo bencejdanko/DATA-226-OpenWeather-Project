7828f7c8b8ce65c7b9dda59029de1ca7fb45861e
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2024-11-30 22:53:25 -0800

    changed name to city_name

--- weather_dbt/models/output/wind_speed_analysis.sql
+++ weather_dbt/models/output/wind_speed_analysis.sql
@@ -1,7 +1,7 @@
 WITH WindSpeedData AS (
     SELECT 
         w.WIND_SPEED,
-        c."Name" AS CITY_NAME,
+        c.city_name AS CITY_NAME,
         w.DATE_TIME,
         DATE_TRUNC('DAY', w.DATE_TIME) AS DAY
     FROM

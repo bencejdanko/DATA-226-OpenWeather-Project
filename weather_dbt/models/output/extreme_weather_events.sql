02939213cd39f437fc3c01154a77498e6e8180d3
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2024-11-30 23:33:57 -0800

    bugfix: avg for each day weather added

--- weather_dbt/models/output/extreme_weather_events.sql
+++ weather_dbt/models/output/extreme_weather_events.sql
@@ -1,43 +1,51 @@
-WITH Extreme_Weather_Events AS (
+WITH extreme_weather_events AS (
     SELECT
         w.CITY_ID,
         c.city_name AS CITY_NAME,
-        w.DATE_TIME,
+        DATE_TRUNC('DAY', w.DATE_TIME) AS DAY,
         ((w.TEMP - 273.15) * 9/5 + 32) AS TEMP_FAHRENHEIT,
         w.WIND_SPEED,
         CASE
-            WHEN ((w.TEMP - 273.15) * 9/5 + 32) > 100 THEN 'Extreme Heat'
-            WHEN ((w.TEMP - 273.15) * 9/5 + 32) < 32 THEN 'Extreme Cold'
-            WHEN w.WIND_SPEED > 20 THEN 'High Winds'
+            WHEN ((w.TEMP - 273.15) * 9/5 + 32) > 85 THEN 'Hot Day'
+            WHEN ((w.TEMP - 273.15) * 9/5 + 32) < 59 THEN 'Cold Day'
+            WHEN w.WIND_SPEED > 4 THEN 'Breezy Day'
             ELSE 'Normal'
-        END AS WEATHER_ALERT
+        END AS HOURLY_WEATHER_ALERT
     FROM
         {{ source('openweather', 'WEATHER_FACT_TABLE') }} w
     JOIN
         {{ source('openweather', 'CITY_DIMENSION_TABLE') }} c
     ON
         w.CITY_ID = c.CITY_ID
-    WHERE
-        ((w.TEMP - 273.15) * 9/5 + 32) > 100
-        OR ((w.TEMP - 273.15) * 9/5 + 32) < 32
-        OR w.WIND_SPEED > 20
+),
+Daily_Weather AS (
+    SELECT
+        CITY_NAME,
+        DAY,
+        AVG(TEMP_FAHRENHEIT) AS AVG_TEMP_FAHRENHEIT,
+        AVG(WIND_SPEED) AS AVG_WIND_SPEED,
+        -- Classify weather based on the daily average of temperature
+        CASE
+            WHEN AVG(TEMP_FAHRENHEIT) > 85 THEN 'Hot Day'
+            WHEN AVG(TEMP_FAHRENHEIT) < 59 THEN 'Cold Day'
+            WHEN AVG(WIND_SPEED) > 4 THEN 'Breezy Day'
+            ELSE 'Normal'
+        END AS WEATHER_ALERT
+    FROM
+        extreme_weather_events
+    GROUP BY
+        CITY_NAME,
+        DAY
 )
+-- Final Output
 SELECT
     CITY_NAME,
-    DATE_TRUNC('DAY', DATE_TIME) AS DAY,
-    AVG(TEMP_FAHRENHEIT) AS AVG_TEMP_FAHRENHEIT,
-    AVG(WIND_SPEED) AS AVG_WIND_SPEED,
-    CASE
-        WHEN AVG(TEMP_FAHRENHEIT) > 100 THEN 'Extreme Heat'
-        WHEN AVG(TEMP_FAHRENHEIT) < 32 THEN 'Extreme Cold'
-        WHEN AVG(WIND_SPEED) > 20 THEN 'High Winds'
-        ELSE 'Normal'
-    END AS WEATHER_ALERT
+    DAY,
+    AVG_TEMP_FAHRENHEIT,
+    AVG_WIND_SPEED,
+    WEATHER_ALERT
 FROM 
-    Extreme_Weather_Events
-GROUP BY 
-    CITY_NAME,
-    DAY
+    Daily_Weather
 ORDER BY 
     CITY_NAME,
     DAY

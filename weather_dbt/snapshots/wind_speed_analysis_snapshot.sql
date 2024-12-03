a4bb65eed1893e3045d95c873c1d3759f7475199
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2024-12-03 09:52:11 -0800

    config: snapshot directly refers to output model

--- weather_dbt/snapshots/wind_speed_analysis_snapshot.sql
+++ weather_dbt/snapshots/wind_speed_analysis_snapshot.sql
@@ -10,27 +10,6 @@
   )
 }}
 
-WITH WindSpeedData AS (
-    SELECT 
-        w.WIND_SPEED,
-        w.CITY_NAME,
-        w.DATE_TIME,
-        DATE_TRUNC('DAY', w.DATE_TIME) AS DAY
-    FROM
-        {{ ref('openweather_join') }} w
-    WHERE
-        w.WIND_SPEED IS NOT NULL
-)
-SELECT 
-    CITY_NAME,
-    DAY,
-    AVG(WIND_SPEED) AS AVG_WIND_SPEED
-FROM WindSpeedData
-GROUP BY 
-    CITY_NAME,
-    DAY
-ORDER BY 
-    CITY_NAME,
-    DAY
+SELECT * FROM {{ ref('wind_speed_analysis') }}
 
 {% endsnapshot %}

b76451fb235b3c5a2a38012067b641e35f94c8ed
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2024-12-03 09:50:24 -0800

    config: snapshot directly refers to output model

--- weather_dbt/snapshots/correlations_snapshot.sql
+++ weather_dbt/snapshots/correlations_snapshot.sql
@@ -5,26 +5,11 @@
     target_schema='snapshot',
     unique_key='CITY_NAME',  
     strategy='timestamp',
-    updated_at='UPDATED_AT',     
+    updated_at='DAY',     
     invalidate_hard_deletes=True
   )
 }}
 
-WITH CorrelationData AS (
-    SELECT 
-        w.CITY_NAME,
-        DATE_TRUNC('DAY', w.DATE_TIME) AS DAY,
-        AVG(w.TEMP_FAHRENHEIT) AS AVG_TEMP,
-        AVG(w.HUMIDITY) AS AVG_HUMIDITY,
-        AVG(w.PRESSURE) AS AVG_PRESSURE,
-        AVG(w.WIND_SPEED) AS AVG_WIND_SPEED,
-        DATE_TRUNC('DAY', w.DATE_TIME) AS UPDATED_AT
-    FROM
-        {{ ref('openweather_join') }} w
-    GROUP BY 
-        w.CITY_NAME, DATE_TRUNC('DAY', w.DATE_TIME)
-)
-SELECT *
-FROM CorrelationData
+SELECT * FROM {{ ref('correlation') }}
 
 {% endsnapshot %}

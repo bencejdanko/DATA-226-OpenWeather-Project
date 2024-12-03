9c57f2989a2ec94d109f7c770253a57bafc481ca
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2024-12-03 09:51:51 -0800

    config: snapshot directly refers to output model

--- weather_dbt/snapshots/temp_trends_by_city_snapshot.sql
+++ weather_dbt/snapshots/temp_trends_by_city_snapshot.sql
@@ -10,18 +10,6 @@
   )
 }}
 
-WITH TempTrends AS (
-    SELECT 
-        w.CITY_NAME,
-        DATE_TRUNC('DAY', w.DATE_TIME) AS DAY,
-        AVG(w.TEMP_FAHRENHEIT) AS AVG_TEMP_FAHRENHEIT
-    FROM
-        {{ ref('openweather_join') }} w
-    GROUP BY 
-        w.CITY_NAME, DATE_TRUNC('DAY', w.DATE_TIME)
-)
-SELECT *
-FROM TempTrends
-ORDER BY CITY_NAME, DAY
+SELECT * FROM {{ ref('temp_trends_by_city') }}
 
 {% endsnapshot %}

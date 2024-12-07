6db703d50f26c9a8f39724ef16df9654e782e1a2
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2024-12-06 19:32:51 -0800

    config: added snowflake connection

--- dags/load_weather_per_city_realtime.py
+++ dags/load_weather_per_city_realtime.py
@@ -2,36 +2,39 @@ import pandas as pd
 import requests
 from airflow.decorators import task
 from airflow.models import Variable
-from sqlalchemy import create_engine
-from snowflake.sqlalchemy import URL
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 
 import datetime
 import time
 
-def return_snowflake_engine():
-    snowflake_url = URL(
-        user=Variable.get('SNOWFLAKE_USER'),
-        password=Variable.get('SNOWFLAKE_PASSWORD'),
-        account=Variable.get('SNOWFLAKE_ACCOUNT'),
-        warehouse='compute_wh',
-        database='openweather',
-        schema='raw_data'
-    )
-    return create_engine(snowflake_url)
 
+# Function to return a Snowflake cursor
+def return_snowflake_conn():
+    hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+    return hook.get_conn().cursor()
+
+
+# Task to fetch city data from Snowflake
 @task
 def get_city_data():
-    query = 'select * from CITY_DIMENSION_TABLE'
-    engine = return_snowflake_engine()
-    cities_df = pd.read_sql(query, engine)
+    query = 'SELECT * FROM CITY_DIMENSION_TABLE'
+    cursor = return_snowflake_conn()
+    
+    cursor.execute(query)
+    data = cursor.fetchall()
+    columns = [col[0] for col in cursor.description]
+    cities_df = pd.DataFrame(data, columns=columns)
+    
     return cities_df
 
+
+# Task to fetch weather data from OpenWeather API
 @task
 def get_weather_data(cities_df):
     key = Variable.get('OPENWEATHER_API_KEY')
     weather_data = []
 
-    for index, row in cities_df.iterrows():
+    for _, row in cities_df.iterrows():
         city_id = row['city_id']
         city_name = row['Name']
         lat = row['Latitude']
@@ -60,26 +63,50 @@ def get_weather_data(cities_df):
     weather_df = pd.DataFrame(weather_data)
     return weather_df
 
+
+# Task to load weather data into Snowflake
 @task
 def load_weather_data(weather_df):
-    engine = return_snowflake_engine()
-    connection = engine.connect()
+    cursor = return_snowflake_conn()
+
     try:
-        transaction = connection.begin()    # Start Transaction (Similar to BEGIN)
-        weather_df.to_sql('weather_realtime_table', con=connection, index=False, if_exists='replace')
-        transaction.commit()    # Commit Transaction (Similar to COMMIT)
+        # Begin transaction
+        cursor.execute("BEGIN")
+        
+        # Insert data into Snowflake
+        for _, row in weather_df.iterrows():
+            insert_query = f"""
+                INSERT INTO weather_realtime_table (city_id, date_time, temp, feels_like, pressure, humidity, wind_speed, cloud_coverage, weather_main, weather_det)
+                VALUES (
+                    '{row['city_id']}',
+                    '{row['date_time']}',
+                    {row['temp']},
+                    {row['feels_like']},
+                    {row['pressure']},
+                    {row['humidity']},
+                    {row['wind_speed']},
+                    {row['cloud_coverage']},
+                    '{row['weather_main']}',
+                    '{row['weather_det']}'
+                )
+            """
+            cursor.execute(insert_query)
+        
+        # Commit transaction
+        cursor.execute("COMMIT")
     except Exception as e:
-        transaction.rollback()
+        cursor.execute("ROLLBACK")
         print(f"Error occurred: {e}")
     finally:
-        connection.close()      # Close the connection
+        cursor.close()
+
 
 # Example DAG definition
 from airflow import DAG
 
 with DAG(
     'load_weather_per_city_realtime',
-    start_date= datetime.datetime(2024,9,1),
+    start_date=datetime.datetime(2024, 9, 1),
     schedule_interval='@hourly',
     tags=['ETL', 'RealTime'],
     catchup=False
